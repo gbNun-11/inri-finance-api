299fef538c3e624f2a0ef4f0dc19cffc09862c66
Gabriel Nunes <gabriel.nunesantoscoelho@gmail.com>
2026-04-24 11:31:41 -0300

    docs: add api/users/balance  to swagger docs

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,8 +5,8 @@ const router = new Router()
 const userController = makeGetUserController()
 
 // Routes
-router.get('/:userId', (req, res) => userController.show(req, res))
 router.get('/:userId/balance', (req, res) => userController.index(req, res))
+router.get('/:userId', (req, res) => userController.show(req, res))
 router.patch('/:userId', (req, res) => userController.update(req, res))
 router.delete('/:userId', (req, res) => userController.delete(req, res))
 router.post('/', (req, res) => userController.store(req, res))
