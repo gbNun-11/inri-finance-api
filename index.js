8ce6865fe55a4d673e0eba932abd644de93030ab
Gabriel Nunes <gabriel.nunesantoscoelho@gmail.com>
2026-04-29 09:54:21 -0300

    refactor: create npm run start

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config.js'
 import app from './app.js'
 
-const port = process.env.PORT
+const port = process.env.PORT || 8030
 app.listen(port, () => {
   console.log()
   console.log(`Listening on Port: ${port}`)
